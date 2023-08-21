a1b92c354a28773c857535456ebe4acc6b8c0e5a
Aayush Rijal <arijal@aiims.com.au>
2023-08-21 14:37:26 +1000

    added script

--- src/theme-files/parts/section-nav.php
+++ src/theme-files/parts/section-nav.php
@@ -47,13 +47,24 @@
                             </a>
                         </div>
                         <div class="w-fit px-4">
-                            <a href="javascript:void(0)" id="openGiantMenu" class="font-articulat font-semibold text-base flex items-center justify-center gap-x-3 bg-dark-blue rounded-md h-[41px] px-3.5">
-                                <svg width="27" height="24" viewBox="0 0 27 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <rect x="0.326172" y="0.865723" width="25.8588" height="2.67134" rx="1.33567" fill="#FFAE35" />
-                                    <rect x="9.01172" y="10.6553" width="17.1741" height="2.67134" rx="1.33567" fill="#FFAE35" />
-                                    <rect x="0.326172" y="20.4448" width="25.8588" height="2.67134" rx="1.33567" fill="#FFAE35" />
-                                </svg>
-                            </a>
+                            <div class="flex items-center gap-x-3">
+                                <div class="w-fit md:hidden">
+                                    <a href="tel:<?= get_field('phone_number', 'options') ?>">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#f4a938" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+                                        </svg>
+                                    </a>
+                                </div>
+                                <div class="w-fit">
+                                    <a href="javascript:void(0)" id="openGiantMenu" class="font-articulat font-semibold text-base flex items-center justify-center gap-x-3 bg-dark-blue rounded-md h-[41px] px-3.5">
+                                        <svg width="35" height="32" viewBox="0 0 35 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <rect x="0.326172" y="0.865723" width="25.8588" height="2.67134" rx="1.33567" fill="#FFAE35" />
+                                            <rect x="9.01172" y="10.6553" width="17.1741" height="2.67134" rx="1.33567" fill="#FFAE35" />
+                                            <rect x="0.326172" y="20.4448" width="25.8588" height="2.67134" rx="1.33567" fill="#FFAE35" />
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
